c8865241ffd954e18e71a0a430284db0dc226f8f
Paula Klinke <Paula.Klinke@student.hpi.de>
2021-11-09 14:39:42 +0100

    Add form for adding new authors

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -7,6 +7,7 @@ describe "New author page", type: :feature do
    end
 
    it "should have text inputs for name, surname and homepage" do
+     visit new_author_path
      expect(page).to have_field('author[first_name]')
      expect(page).to have_field('author[last_name]')
      expect(page).to have_field('author[homepage]')
